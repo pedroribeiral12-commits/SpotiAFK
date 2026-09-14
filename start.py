e03e69728aa06e87e735008d414a899eb8703c3c
pedroribeiral12-commits <pedroribeiral12@gmail.com>
2026-09-14 22:51:12 +0100

    Update start.py

--- start.py
+++ start.py
@@ -7,13 +7,14 @@ client_id = os.getenv("CLIENT_ID", "").strip()
 client_secret = os.getenv("CLIENT_SECRET", "").strip()
 raw_cache = os.getenv("SPOTIFY_CACHE", "").strip()
 
-# Garantir um local previsível para os dados do SpotiAFK
+# Diretório de dados do SpotiAFK no Render
 data_home = Path("/opt/render/project/src/.local/share")
 state_dir = data_home / "spotiafk"
 
 os.environ["XDG_DATA_HOME"] = str(data_home)
 
-config_content = f"""playlist = "Best drake songs ever"
+# Configuração
+config_content = f"""playlist = "6rir2yuNUI8p7jjpTvi2wE"
 play_on = ["iPhone"]
 
 [spotify]
@@ -26,10 +27,12 @@ state_dir = "{state_dir}"
 with open("spotiafk.toml", "w", encoding="utf-8") as f:
     f.write(config_content)
 
+# Restaurar cache do Spotify
 if not raw_cache:
     print("--> ERRO: SPOTIFY_CACHE nao encontrada!", flush=True)
 else:
     try:
+        # Remove aspas exteriores, caso existam
         if (
             (raw_cache.startswith('"') and raw_cache.endswith('"'))
             or
@@ -52,6 +55,7 @@ else:
     except Exception as e:
         print(f"--> ERRO ao processar SPOTIFY_CACHE: {e}", flush=True)
 
+# Iniciar SpotiAFK
 subprocess.run(
     ["poetry", "run", "python", "-m", "spotiafk", "run"],
     check=False
