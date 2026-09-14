c3626c7640dc1e175ce732d10d67e69f424306a3
pedroribeiral12-commits <pedroribeiral12@gmail.com>
2026-09-14 21:49:54 +0100

    Create start.py

---
+++ start.py
@@ -0,0 +1,19 @@
+import os
+import subprocess
+
+client_id = os.getenv("CLIENT_ID", "")
+client_secret = os.getenv("CLIENT_SECRET", "")
+
+config_content = f"""[spotify]
+client_id = "{client_id}"
+client_secret = "{client_secret}"
+redirect_uri = "http://127.0.0.1:8888/callback"
+playlist = "Best drake songs ever"
+play_on = ["iPhone"]
+"""
+
+with open("spotiafk.toml", "w") as f:
+    f.write(config_content)
+
+print("spotiafk.toml generated successfully!")
+subprocess.run(["poetry", "run", "python", "-m", "spotiafk", "run"])
